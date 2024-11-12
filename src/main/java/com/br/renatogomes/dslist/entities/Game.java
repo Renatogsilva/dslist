15ac2e9668043e89ec6fcf37e51d6b507c799c0f
Renato Gomes <silvarenato180@gmail.com>
2024-11-12 08:33:02 -0300

    Criação da classe game

---
+++ src/main/java/com/br/renatogomes/dslist/entities/Game.java
@@ -0,0 +1,31 @@
+package com.br.renatogomes.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Data
+@Entity
+@Table(name = "tb_game")
+public class Game implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+}
